9a5194802802f9e5983a1dc1f8c51701d7fa7564
Kyle Mathews <mathews.kyle@gmail.com>
2022-04-11 14:36:31 -0700

    Move back to createPages cause no inc updates

---
+++ gatsby-node.js
@@ -0,0 +1,48 @@
+const path = require(`path`)
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+
+  // Define a template for blog post
+  const blogPost = path.resolve(`./src/templates/blog-post.js`)
+
+  const result = await graphql(
+    `
+      {
+        allDatoCmsPost {
+          nodes {
+            id
+            slug
+          }
+        }
+      }
+    `
+  )
+
+  if (result.errors) {
+    reporter.panicOnBuild(
+      `There was an error loading your DatoCMS posts`,
+      result.errors
+    )
+    return
+  }
+
+  const posts = result.data.allDatoCmsPost.nodes
+
+  // Create blog posts pages
+  // But only if there's at least one blog post found in Contentful
+  // `context` is available in the template as a prop and as a variable in GraphQL
+
+  if (posts.length > 0) {
+    posts.forEach((post) => {
+      createPage({
+        path: `/posts/${post.slug}/`,
+        component: blogPost,
+        ownerNodeId: post.id,
+        context: {
+          id: post.id,
+        },
+      })
+    })
+  }
+}
